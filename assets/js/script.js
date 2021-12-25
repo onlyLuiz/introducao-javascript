42d0c4da59c6074bbc53baf1cd598656cc9319db
Luiz Eduardo da Silva Araújo <luizsilvaa1114@gmail.com>
2021-12-25 02:48:47 -0300

    adicionando assets:css and javascript

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+
+function increment() {
+        currentNumber = currentNumber + 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+
+        if (currentNumber >= 100) {
+          currentNumberWrapper.innerHTML = 0;
+          currentNumber = 0;
+        }
+
+        var color = document.getElementById('currentNumber');
+        if (currentNumber >= 1) {
+        color.style.color = "grey"; 
+    }
+
+}
+
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+
+    if (currentNumber <= -50) {
+      currentNumberWrapper.innerHTML = 0;
+      currentNumber = 0;
+    }
+
+    var color = document.getElementById('currentNumber');
+    if (currentNumber <= -1) {
+    color.style.color = "red"; 
+}
+}
+
+function modifyNumber() {
+  var number =  document.getElementById("currentNumber");
+  number.firstChild.nodeValue = "aperte algum butão";
+}
+
+function change() {
+    var mood = document.getElementById("container");
+    mood.addEventListener("click", modifyNumber, false);
+  }
+  
+  document.addEventListener("DOMContentLoaded", change, false);
